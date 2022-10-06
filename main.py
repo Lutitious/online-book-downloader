28531dc8fcbec048afc63a3e6bc871c1d5251750
Rahul Imran <notlutitious@gmail.com>
2022-10-06 14:25:29 +0100

    brejgkhrjkoghnwklsd

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from pydoc import pager
 import requests
 
 pageNum = 0
-for i in range(0,120):
+for i in range(1,208):
     pageNum = i * 2
-    pageUrl = "https://illuminate.digital/books/Psychology/aqapsych1/"+str(pageNum)+"-"+str(pageNum+1)+"/"+str(pageNum)+"-"+str(pageNum+1)+".jpg"
+    #https://illuminate.digital/books/Psychology/aqapsych2/2-3/2.jpg
+    pageUrl = "https://illuminate.digital/books/Psychology/aqapsych2/"+str(pageNum)+"-"+str(pageNum+1)+"/"+str(pageNum)+".jpg"
     pageFile = requests.get(pageUrl)
     open("pages/page"+str(pageNum)+".jpg","wb").write(pageFile.content)
     print("Page "+str(pageNum)+" downloaded")
